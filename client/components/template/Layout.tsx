a2d40b112a1a78cb3b518b52766b765a481b31e9
박민석 <pms16011172@gmail.com>
2022-10-18 19:45:15 +0900

    [DESIGN] 프로젝트 기본 레이아웃 padding 설정

--- client/components/template/Layout.tsx
+++ client/components/template/Layout.tsx
@@ -1,5 +1,23 @@
-import React from 'react';
+import styled from '@emotion/styled';
+import React, { PropsWithChildren } from 'react';
+import HeaderNav from '../molecule/HeaderNav';
 
-export default function Layout() {
-  return <div>Layout</div>;
+const BaseLayout = styled.div`
+  min-width: 1440px;
+  width: 100%;
+  padding-left: 185px;
+  padding-right: 180px;
+  @media (min-width: 1440px) {
+    padding-left: calc((100vw - 1440px) / 2 + 185px);
+    padding-right: calc((100vw - 1440px) / 2 + 180px);
+  }
+`;
+
+export default function Layout({ children }: PropsWithChildren<{}>) {
+  return (
+    <BaseLayout>
+      <HeaderNav></HeaderNav>
+      <div>{children}</div>;
+    </BaseLayout>
+  );
 }
