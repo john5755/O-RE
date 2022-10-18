12a59bf05cee3a2d9566fdcef70e30c86629b2ae
박민석 <pms16011172@gmail.com>
2022-10-18 15:23:52 +0900

    [FEATURE] Heading, textSize, fontColor 상수화 및 폴더 구조 수립 #3

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import { H1, H2, H3, H4 } from '../components/styles';
 
 export default function Home() {
-  return <div>index</div>;
+  return (
+    <>
+      <H1 style={{ color: 'var(--main-color)' }}>O:RE</H1>
+      <H2>O:RE</H2>
+      <H3>O:RE</H3>
+      <H4>O:RE</H4>
+      <div style={{ fontSize: 'var(--body-xl)' }}>O:RE</div>
+      <div style={{ fontSize: 'var(--body-l)' }}>O:RE</div>
+      <div style={{ fontSize: 'var(--body-m)' }}>O:RE</div>
+      <div style={{ fontSize: 'var(--body-s)' }}>O:RE</div>
+    </>
+  );
 }
