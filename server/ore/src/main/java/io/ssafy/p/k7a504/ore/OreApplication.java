97c1b06274c80fe0549be53d6fe83549a3d0ec95
신치용 <cldydtls@naver.com>
2022-10-24 13:09:36 +0900

    [SETTING] Spring Boot 초기 설정 #8 #S07P31A504-71

---
+++ server/ore/src/main/java/io/ssafy/p/k7a504/ore/OreApplication.java
@@ -0,0 +1,13 @@
+package io.ssafy.p.k7a504.ore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OreApplication.class, args);
+	}
+
+}
