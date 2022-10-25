290e8bea95dd76210ae672f0305471574677542f
박민석 <pms16011172@gmail.com>
2022-10-25 17:34:22 +0900

    [FEATURE] 프로젝트 전반적인 Layout 작성 #20 S07P31A504-93

---
+++ client/renderer/molecule/PageSideBar.tsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+import React from "react";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  background: var(--light-main-color);
+  border-radius: 10px 0px 0px 0px;
+`;
+
+export default function PageSideBar() {
+  return <Container>PageSideBar</Container>;
+}
