38e680bd3030e5719870271a9e1fb75fc93a6f3b
김민지 <whiterisi@naver.com>
2022-11-01 17:32:55 +0900

    [REFACTOR] aws.yml  관련 부분 삭제 #43

--- server/ore/src/main/java/io/ssafy/p/k7a504/ore/OreApplication.java
+++ server/ore/src/main/java/io/ssafy/p/k7a504/ore/OreApplication.java
@@ -2,17 +2,12 @@ package io.ssafy.p.k7a504.ore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class OreApplication {
-	public static final String APPLICATION_LOCATIONS = "spring.config.location="
-			+ "classpath:application.yml,"
-			+ "classpath:aws.yml";
+
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(OreApplication.class)
-				.properties(APPLICATION_LOCATIONS)
-				.run(args);
+		SpringApplication.run(OreApplication.class, args);
 	}
 
 }
