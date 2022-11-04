5b2b36b81cfaa4ae9ed04376e204e615e022924b
MINJI <97995420+whiterisi@users.noreply.github.com>
2022-11-04 17:48:38 +0900

    refactor: use jpql

--- server/ore/src/main/java/io/ssafy/p/k7a504/ore/teamUser/repository/TeamUserRepository.java
+++ server/ore/src/main/java/io/ssafy/p/k7a504/ore/teamUser/repository/TeamUserRepository.java
@@ -6,6 +6,8 @@ import io.ssafy.p.k7a504.ore.user.domain.User;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import javax.swing.text.html.Option;
 import java.util.List;
@@ -18,4 +20,9 @@ public interface TeamUserRepository extends JpaRepository<TeamUser, Long> {
     Optional<TeamUser> findByUserIdAndTeamId(Long userId, Long teamId);
     boolean existsByUserAndTeam(User user, Team team);
     boolean existsByUserAndTeamId(User user, Long teamId);
+    @Query("select tu from TeamUser tu join fetch User u where u.name like %:name%")
+    Slice<TeamUser> findTeamUserByUsername(@Param("name") String name);
+
+    @Query("select tu from TeamUser tu join fetch User u where u.nickname like %:name%")
+    Slice<TeamUser> findTeamUserByUserNickname(@Param("name") String nickname);
 }
